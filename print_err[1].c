a4b5f1abfb6997303068ee367be49d8b07170c80
edzordzinam44 <edzordzinam.alipui@gmail.com>
2023-11-20 01:55:10 +0000

    print error

---
+++ print_err[1].c
@@ -0,0 +1,120 @@
+#include "monty.h"
+
+/**
+ * err - Prints appropriate error messages based on their error code.
+ *
+ * @error_code: The error codes and their meanings:
+ * (1) => No file or more than one file provided to the program.
+ * (2) => The provided file cannot be opened or read.
+ * (3) => Invalid instruction in the provided file.
+ * (4) => Memory allocation failure (malloc).
+ * (5) => "Push" instruction parameter is not an integer.
+ * (6) => Stack is empty for "pint" operation.
+ * (7) => Stack is empty for "pop" operation.
+ * (8) => Stack is too short for the specific operation.
+ */
+
+void err(int error_code, ...)
+{
+	va_list ag;
+	char *op;
+	int l_num;
+
+	va_start(ag, error_code);
+	switch (error_code)
+	{
+		case 1:
+			fprintf(stderr, "USAGE: monty file\n");
+			break;
+		case 2:
+			fprintf(stderr, "Error: Can't open file %s\n",
+				va_arg(ag, char *));
+			break;
+		case 3:
+			l-num = va_arg(ag, int);
+			op = va_arg(ag, char *);
+			fprintf(stderr, "L%d: unknown instruction %s\n", l_num, op);
+			break;
+		case 4:
+			fprintf(stderr, "Error: malloc failed\n");
+			break;
+		case 5:
+			fprintf(stderr, "L%d: usage: push integer\n", va_arg(ag, int));
+			break;
+		default:
+			break;
+	}
+	free_nodes();
+	exit(EXIT_FAILURE);
+}
+
+/**
+ * more_err - Handles specific errors and their associated codes.
+ *
+ * @error_code: Error codes and their meanings:
+ * (6) => Stack is empty for "pint" operation.
+ * (7) => Stack is empty for "pop" operation.
+ * (8) => Stack is too short for the intended operation.
+ * (9) => Division by zero error.
+ */
+void more_err(int error_code, ...)
+{
+	va_list ag;
+	char *op;
+	int l_num;
+
+	va_start(ag, error_code);
+	switch (error_code)
+	{
+		case 6:
+			fprintf(stderr, "L%d: can't pint, stack empty\n",
+				va_arg(ag, int));
+			break;
+		case 7:
+			fprintf(stderr, "L%d: can't pop an empty stack\n",
+				va_arg(ag, int));
+			break;
+		case 8:
+			l_num = va_arg(ag, unsigned int);
+			op = va_arg(ag, char *);
+			fprintf(stderr, "L%d: can't %s, stack too short\n", l_num, op);
+			break;
+		case 9:
+			fprintf(stderr, "L%d: division by zero\n",
+				va-arg(ag, unsigned int));
+			break;
+		default:
+			break;
+	}
+	free_nodes();
+	exit(EXIT_FAILURE);
+}
+
+/**
+ * string_err - handles errors.
+ * @error_code: The error codes are the following:
+ * (10) ~> The number inside a node is outside ASCII bounds.
+ * (11) ~> The stack is empty.
+ */
+
+void string_err(int error_code, ...)
+{
+	va_list ag;
+	int l_num;
+
+	va_start(ag, error_code);
+	l_num = va_arg(ag, int);
+	switch (error_code)
+	{
+		case 10:
+			fprintf(stderr, "L%d: can't pchar, value out of range\n", l_num);
+			break;
+		case 11:
+			fprinf(stderr, "L%d: can't pchar, stack empty\n", l_num);
+			break;
+		default:
+			break;
+	}
+	free_nodes();
+	exit(EXIT_FAILURE);
+}
